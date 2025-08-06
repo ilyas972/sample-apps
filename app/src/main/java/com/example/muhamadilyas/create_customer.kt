34e72d1ef50436440bf6a6966fa9d100b6141278
Fahmi XD II <contributor404@gmail.com>
2025-08-06 11:59:39 +0700

    fist comit

---
+++ app/src/main/java/com/example/muhamadilyas/create_customer.kt
@@ -0,0 +1,20 @@
+package com.example.muhamadilyas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class create_customer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_create_customer)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
